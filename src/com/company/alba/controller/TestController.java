15f98d8f1d22c77287d7604b6e26efd87499eecb
bturekhanov <bturkbtu@gmail.com>
2013-05-02 23:06:29 -0800

    testing Dymanic web project

---
+++ src/com/company/alba/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.company.alba.controller;
+
+public class TestController {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hi from console!!!");
+	}
+
+}
